43ae4f9da58bf5e21e9c5e5b65c59f64e6f9aece
Tomislav <greatins.test@gmail.com>
2020-12-13 01:55:41 +0100

    adding tsudar1991 as attendee

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -15,39 +15,44 @@ class ScheduleController extends Controller
 {
     public function store(Request $request)
     {
+        
         if ($request->address)
          return redirect('/schedule')->with('success','Event Created');
 
         $this->validate($request,[
-            'name'=>['required','alpha'],
+            'name'=>['required','regex:/^[\pL\s\-]+$/u'],
             'phone'=>'required',
             'email'=>'required',
             'title'=>'required',
             'date'=>'required']);
+        
         $name=$request->input('name');
         $phone=$request->input('phone');
         $email=$request->input('email');
         $date = $request->input('date');
         $time = $request->input('time');
+
         $start = Carbon::parse($date.' '.$time,'Europe/Belgrade');
         $end = (clone $start)->addHour();
-        $event = new Event;
-        $event->name = $name;
-            $event->description = 'Event for Great Services d.o.o. home test';
-            $event->startDateTime = $start;
-            $event->endDateTime = $end;
-            $event->addAttendee(['email' => $email,'comment'=>'Name: Tomislav Sudar tel:'.$phone]);
-            $event->sendUpdates ='all';
-            $event->reminders=array(
-                'useDefault' => FALSE,
-                'overrides' => array(
-                  array('method' => 'email', 'minutes' => 60),
-                  array('method' => 'email', 'minutes' => 30),
-                ),);
-            
-            $event->save('insertEvent',array('sendNotifications' => true, 'conferenceDataVersion' => true));
         
-      return redirect('/schedule')->with('success','Event Created');
+        $event = new Event;
+        $event->name = $title;
+        $event->description = 'Event for Great Services d.o.o. home test';
+        $event->startDateTime = $start;
+        $event->endDateTime = $end;
+        $event->addAttendee(['email' => $email,'comment'=>'Name: '.$name.'; tel: '.$phone]);
+        $event->addAttendee(['email' => 'tsudar1991@gmail.com','comment'=>'Name: Tomislav Sudar; tel: 0638043816']);
+        $event->sendUpdates ='all';
+        $event->reminders=array(
+         'useDefault' => FALSE,
+         'overrides' => array(
+            array('method' => 'email', 'minutes' => 60),
+            array('method' => 'email', 'minutes' => 30),)
+        );
+            
+        $event->save('insertEvent',array('sendNotifications' => true, 'conferenceDataVersion' => true));
+        
+        return redirect('/schedule')->with('success','Event Created');
         
         
     }

8bf18d0a92ef2aa163f893c2488bbc723f12a457
Tomislav <greatins.test@gmail.com>
2020-12-13 02:18:27 +0100

    Adding git link to home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
     @section('content')
      <div class="jumbotron text-center">
-        <h1>Greatins Google Calendar Project</h1>
-        <p>To test Google calendar API go to schedule. Click on the download button to get repository.</p>
-        <p><a class="btn btn-primary btn-lg" href="/schedule" role="button">Schedule</a> <a class="btn btn-success btn-lg" href="" role="button">Download</a></p>
+        <h1>Greatins Google Calendar API Home Test</h1>
+        <p>To test Google calendar API go to Create Event below. For source code, click the download button below to get repository.</p>
+        <p><a class="btn btn-primary btn-lg" href="/schedule" role="button">Create Event</a> <a class="btn btn-success btn-lg" href="https://github.com/greatinstest/greatins" role="button">Download</a></p>
      </div>
     @endsection
